954d860c0ec8451d3e4eb2dc4aeb0a9950c5584c
juleroliveira <juleroliveira>
2022-08-11 14:52:20 -0300

    added mplayer chiptune playlist

--- desktops/openbox/desktop-settings.sh
+++ desktops/openbox/desktop-settings.sh
@@ -43,9 +43,9 @@ sudo cp -R $path_pool/artwork/fonts/* /usr/share/fonts/truetype/
 echo "Copiando ícones..."
 sudo cp -R $path_pool/artwork/icons/* /usr/share/icons/
 
-echo "Copiando playlist chiptune'
-sudo cp -R $path_poll/bin/chiptune.pls $HOME
-sudo cp -R $path_poll/bin/.openmplayer.sh $HOME
+echo "Copiando playlist chiptune"
+sudo cp -R $path_pool/bin/chiptune.pls $HOME
+sudo cp -R $path_pool/bin/.openmplayer.sh $HOME
 
 echo "Copiando temas do Rofi..."
 sudo cp -R $HOME/.config/rofi/themes/* /usr/share/rofi/themes/
